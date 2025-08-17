6a0f9988db4475dfadd8a251e9e2b6e43621a887
Alessandro Azevedo_ Solved <tech-admin@icpf.org.br>
2025-08-17 03:19:43 +0000

    Nas abas CARACTERIZAÇÃO SERVIÇOS AMBIENTAIS RANKING DE ESPÉCIES Crie um

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-auto items-center justify-center p-0 text-muted-foreground bg-transparent border-b",
+      "inline-flex h-auto items-center justify-center rounded-none border-b bg-transparent p-0 text-muted-foreground",
       className
     )}
     {...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-none px-3 py-2.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-b-2 border-transparent data-[state=active]:text-primary data-[state=active]:border-primary data-[state=active]:shadow-none",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-b-2 border-transparent data-[state=active]:bg-transparent data-[state=active]:text-primary data-[state=active]:border-primary data-[state=active]:shadow-none hover:bg-accent hover:text-accent-foreground",
       className
     )}
     {...props}
