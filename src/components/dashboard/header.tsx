bbd4cec73b00c5317cd03f9ab617e88db2640c2a
Alessandro Azevedo_ Solved <tech-admin@icpf.org.br>
2025-08-27 14:56:43 +0000

    mantenha os botões do head no canto direito da pltaforma apenas com um m

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -49,7 +49,7 @@ export default function DashboardHeader() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <Logo className="ml-8" />
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <div className="flex flex-1 items-center justify-end space-x-2 mr-8">
           <nav className="hidden items-center space-x-4 sm:flex">
             <AboutDialog>
               <Button variant="ghost">{t('about')}</Button>
