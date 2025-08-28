d81cb93fa39344341775c37ab85bb992533f0924
Alessandro Azevedo_ Solved <tech-admin@icpf.org.br>
2025-08-28 13:53:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCK06MasinVgp_lqAr0tBr4wWKl6XRgRM4",
-  authDomain: "biodiversidade2.firebaseapp.com",
-  projectId: "biodiversidade2",
-  storageBucket: "biodiversidade2.firebasestorage.app",
-  messagingSenderId: "134238563596",
-  appId: "1:134238563596:web:162c679e2cae78c31e2d29"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
